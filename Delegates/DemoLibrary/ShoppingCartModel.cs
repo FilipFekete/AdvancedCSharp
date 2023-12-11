3df66cada7831489197cbb4c0370e50f553eb7fa
FilipFekete <fifek23@student.sdu.dk>
2023-12-11 04:18:37 +0100

    m

---
+++ Delegates/DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+
+        public delegate void MentionDiscount(decimal subTotal);
+
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+        
+        //generating total with discount
+        public decimal GenerateTotal(MentionDiscount mentionSubtotal,
+           Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal,
+           Action<string> tellUserDiscount)
+            
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            mentionSubtotal(subTotal);
+
+            tellUserDiscount("We are applying your discount.");
+
+            decimal total = calculateDiscountedTotal(Items, subTotal);
+
+            return total;         
+        }
+    }
+}
