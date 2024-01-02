526b3bc5618af2ba9d328ad625119fb7cbc3a9e2
FilipFekete <fifek23@student.sdu.dk>
2024-01-02 22:21:00 +0100

    Understanding abstract classes

---
+++ AbstractClasses/DemoLibrary/DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public abstract class DataAccess
+    {   
+        // Marking DataAccess as abstract prohibits any instantiation 
+        // Cannot create instance but can access anyclass that inherits from DataAccess
+        public virtual string LoadConnectionString(string name)
+        {
+            Console.WriteLine("Load Connection String");
+            return "testConnectionString";
+        }
+        public abstract void LoadData(string sql);
+
+        public abstract void SaveData(string sql);
+    }
+}
---
+++ AbstractClasses/DemoLibrary/SqliteDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SqliteDataAccess : DataAccess
+    {
+        public override string LoadConnectionString(string name)
+        {
+            string output = base.LoadConnectionString(name);
+
+            output += " (from SQLite)";
+
+            return output;
+        }
+
+        public override void LoadData(string sql)
+        {   
+            // Marking method LoadData as override creates an initialization of LoadData - adds body to the method
+            Console.WriteLine("Loading SQLite Data");
+        }
+
+        public override void SaveData(string sql)
+        {
+            Console.WriteLine("Saving data to SQLite");
+        }
+    }
+}
