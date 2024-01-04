eae1dee23b1ed22b3246f9ff3fa7c370964478b0
FilipFekete <fifek23@student.sdu.dk>
2024-01-05 00:30:53 +0100

    Using generics to proccess cvs files

---
+++ Generics/ConsoleUI/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //List<int> ages = new List<int>();
+
+            //ages.Add(23);
+
+            DemonstrateTextFileStorage();
+            
+
+            Console.WriteLine();
+            Console.Write("Press enter to shut down...");
+            Console.ReadLine();
+        }
+
+        private static void DemonstrateTextFileStorage()
+        {
+            List<Person> people = new List<Person>();
+            List<LogEntry> logs = new List<LogEntry>();
+
+            string peopleFile = @"C:\Users\fifof\OneDrive\Počítač\CSharpGitHub\AdvancedCSharp\Generics\Temp\people.csv";
+            string logFile = @"C:\Users\fifof\OneDrive\Počítač\CSharpGitHub\AdvancedCSharp\Generics\Temp\logs.csv";
+
+            PopulateLists(people, logs);
+
+            /** Using Generics **/
+
+            GenericTextFileProcessor.SaveToTextFile<Person>(people, peopleFile);
+            GenericTextFileProcessor.SaveToTextFile<LogEntry>(logs, logFile);
+
+            var newPeople = GenericTextFileProcessor.LoadFromTextFile<Person>(peopleFile);
+
+            foreach (var p in newPeople)
+            {
+                Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+            }
+
+            var newLogs = GenericTextFileProcessor.LoadFromTextFile<LogEntry>(logFile);
+
+            foreach (var log in newLogs)
+            {
+                Console.WriteLine($"{log.ErrorCode}: {log.Message} at {log.TimeOfEvent.ToShortTimeString()}");
+            }
+
+            /** Non - Generics **/
+
+            //OriginalTextFileProcessor.SaveLogs(logs, logFile);
+
+            //var newLogs = OriginalTextFileProcessor.LoadLogs(logFile);
+
+            //foreach(var log in newLogs)
+            //{
+            //    Console.WriteLine($"{ log.ErrorCode }: { log.Message } at { log.TimeOfEvent.ToShortTimeString() }");
+            //}
+
+            //OriginalTextFileProcessor.SavePeople(people, peopleFile);
+
+            //var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+
+            //foreach (var p in newPeople)
+            //{
+            //    Console.WriteLine($"{ p.FirstName } { p.LastName } (IsAlive = { p.IsAlive })");
+            //}
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        {
+            people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+            people.Add(new Person { FirstName = "Sue", LastName = "Storm", IsAlive = false });
+            people.Add(new Person { FirstName = "Greg", LastName = "Olsen" });
+
+            logs.Add(new LogEntry { Message = "I blew up", ErrorCode = 9999 });
+            logs.Add(new LogEntry { Message = "I'm too awesome", ErrorCode = 1337 });
+            logs.Add(new LogEntry { Message = "I was tired", ErrorCode = 2222 });
+        }
+    }
+}
